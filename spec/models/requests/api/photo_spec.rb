74bc4647760f4c9f3bf91d733b8bfe7d259baffa
teddycoleman <teddy.f.coleman@gmail.com>
2016-11-19 11:44:17 -0800

    adds tests for field values to the test for photo

--- spec/models/requests/api/photo_spec.rb
+++ spec/models/requests/api/photo_spec.rb
@@ -4,10 +4,13 @@ describe "Testing the photo api", type: :request do
 	before(:each) do
 		@user = User.create(first_name:"Pablo", last_name:"Picasso", email: "pp@gmail.com", password:"swordfish")	
 		@profile = Profile.create(user_id: @user.id, name: "Farleys", style: "Coffeeshop", profile_type: "gallery",description: "great coffeshop")	
+		@photo = @profile.photos.create(price: 200, description: "Awesome painting", order:1)
 	end
 	it "GET /profiles/:id/photos returns a status code of 200" do
 		get "/profiles/#{@profile.id}/photos"
 
+		parsed_body = JSON.parse(response.body)
+		expect(parsed_body[0]["description"]).to eq("Awesome painting")
 		expect(response).to have_http_status(200)
 	end
 	it "POST /profiles/:profile_id/photos returns a status code of 201" do
@@ -23,19 +26,21 @@ describe "Testing the photo api", type: :request do
 			"Authorization": "Token token=#{@user.auth_token}"
 		}
 
+		parsed_body = JSON.parse(response.body)
+		expect(parsed_body["description"]).to eq("Awesome painting")
 		expect(response).to have_http_status(201)
 	end
 
 	it "GET /profiles/:profile_id/photos/:id should return 200 status code" do
-		photo = @profile.photos.create(price: 200, description: "Awesome painting", order:1)
-		get "/profiles/#{@profile.id}/photos/#{photo.id}"
+		get "/profiles/#{@profile.id}/photos/#{@photo.id}"
 
+		parsed_body = JSON.parse(response.body)
+		expect(parsed_body["description"]).to eq("Awesome painting")
 		expect(response).to have_http_status(200)
 	end
 
 	it "PUT /profiles/:profile_id/photos/:id should return 200 status code" do
-		photo = @profile.photos.create(price: 200, description: "Awesome painting", order:1)
-		put "/profiles/#{@profile.id}/photos/#{photo.id}", params: {
+		put "/profiles/#{@profile.id}/photos/#{@photo.id}", params: {
 			photo: {
 				description: "REALLY awesome painting"
 			}
@@ -44,6 +49,8 @@ describe "Testing the photo api", type: :request do
 			"Authorization": "Token token=#{@user.auth_token}"
 		}
 
+		parsed_body = JSON.parse(response.body)
+		expect(parsed_body["description"]).to eq("REALLY awesome painting")
 		expect(response).to have_http_status(200)
 	end
 
