695ce46e9e8d44631e46b7387e1018bc3078bc13
teddycoleman <teddy.f.coleman@gmail.com>
2016-11-21 09:40:46 -0800

    adds photo to photos model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,5 @@
 class Photo < ApplicationRecord
   belongs_to :profile
+  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
