590ed9ee8d5e50367d74074506238bbd9a1a03f9
teddycoleman <teddy.f.coleman@gmail.com>
2016-11-23 08:59:39 -0800

    adds all_photos route to get photos regardless of profile

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 		resources :photos, except: [:edit, :new]
 	end
 
+	get "/photos", to: "photos#all_photos"
+
 	get "/profiles/:profile_id/showings", to: "showings#index"
 	post "/profiles/:profile_id/showings", to: "showings#create"
 	delete "/showings/:id", to: "showings#destroy"
